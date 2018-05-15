c69e72f6469f7d14fa182c035abc2db3b3018f54
Nazarov Andrey <nazarov.andrey@me.com>
2018-05-16 02:24:00 +0300

    export script

--- Assets/MetaWear/Android/MetaWearManager.cs
+++ Assets/MetaWear/Android/MetaWearManager.cs
@@ -7,12 +7,12 @@ namespace MetaWear.Android
     public class MetaWearManager : IMetaWearManager
     {
         private static string TAG = "Android.MetaWearManager";
-        private AndroidJavaObject activity;
+        private AndroidJavaObject manager;
 
         public MetaWearManager ()
         {
-            AndroidJavaClass jc = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-            activity = jc.GetStatic<AndroidJavaObject> ("currentActivity");
+            AndroidJavaClass jc = new AndroidJavaClass ("de.horizont.metawearunity.MetaWearManager");
+            manager = jc.CallStatic<AndroidJavaObject> ("GetInstance");
         }
 
         public IBoard GetBoard (BluetoothDevice bluetoothDevice)
@@ -23,7 +23,7 @@ namespace MetaWear.Android
         public IBoard GetBoard (string macAddress)
         {
             Debug.unityLogger.Log (TAG, "GetBoard");
-            return new Board (activity.Call<AndroidJavaObject> ("GetBoard", macAddress));
+            return new Board (manager.Call<AndroidJavaObject> ("GetBoard", macAddress));
         }
 
         public void ConnectoToTheBoard (IBoard board)
@@ -32,7 +32,7 @@ namespace MetaWear.Android
 
             Board androidBoard = GetBoard (board);
 
-            activity.Call (
+            manager.Call (
                 "ConnectToTheBoard",
                 androidBoard.NativeBoard,
                 androidBoard);
@@ -43,25 +43,25 @@ namespace MetaWear.Android
             Debug.unityLogger.Log (TAG, "GetAccelerometer");
 
             return new Accelerometer (
-                activity.Call<AndroidJavaObject> ("GetAcceleromenter", GetBoard (board).NativeBoard));
+                manager.Call<AndroidJavaObject> ("GetAcceleromenter", GetBoard (board).NativeBoard));
         }
 
         public IMagnetometer GetMagnetometer (IBoard board)
         {
             return new Magnetometer (
-                activity.Call<AndroidJavaObject> ("GetMagnetometer", GetBoard (board).NativeBoard));
+                manager.Call<AndroidJavaObject> ("GetMagnetometer", GetBoard (board).NativeBoard));
         }
 
         public IGyro GetGyro (IBoard board)
         {
             return new Gyro (
-                activity.Call<AndroidJavaObject> ("GetGyro", GetBoard (board).NativeBoard));
+                manager.Call<AndroidJavaObject> ("GetGyro", GetBoard (board).NativeBoard));
         }
 
         public ILight GetLight (IBoard board)
         {
             return new Light (
-                activity.Call<AndroidJavaObject> ("GetLight", GetBoard (board).NativeBoard));
+                manager.Call<AndroidJavaObject> ("GetLight", GetBoard (board).NativeBoard));
         }
 
         private Board GetBoard (IBoard board)
