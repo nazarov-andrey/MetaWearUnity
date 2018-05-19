86679b6a505b438afa2a972160d71def819fdd74
Nazarov Andrey <nazarov.andrey@me.com>
2018-05-19 03:59:26 +0300

    fixes

--- export.sh
+++ export.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchMode -projectPath `pwd` -exportPackage Assets/MetaWear Assets/Plugins/Android/MetaWear ./MetaWear.unitypackage
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchMode -projectPath `pwd` -exportPackage Assets/MetaWear Assets/Plugins/Android ./MetaWear.unitypackage
